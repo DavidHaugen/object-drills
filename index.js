577a67a7ad02069e94f4ea32ecc44beea437c2c8
David Haugen <haugen.dj@gmail.com>
2019-01-17 17:52:54 -0600

    add two drills

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// let loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function(){
+//     return this.water / this.flour * 100;
+//   }
+// };
+
+// console.log(loaf.hydration());
+
+
+// let fooObj = {
+//   foo: 'foo',
+//   bar: 'bar',
+//   fum: 'fum',
+//   quux: 'quux',
+//   spam: 'spam',
+// };
+
+// for (const item in fooObj){
+//   console.log(`${item}: ${fooObj[item]}`);
+// }
